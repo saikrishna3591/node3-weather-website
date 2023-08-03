790064c1f155cf107a1a06c12a6d2afd24d6688f
saikrishnapsk <saikrishnapsk17>
2023-08-03 11:03:34 +0530

    Removed un-necessary console.log calls

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,7 +1,3 @@
-
-
-console.log('Client side javascript file has loaded')
-
 // fetch('https://puzzle.mead.io/puzzle').then((response) =>{
 //     response.json().then((data)=>{
 //         console.log(data)
